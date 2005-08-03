9f9c39dcbcc58200479e606188bcf4deba3c2b94
Simon Strandgaard <neoneye@svn.textmate.org>
2005-08-03 02:16:43 +0000

    browsing through bunches of ruby code in order to stress textmate's coloring.. I saw that there were problems recognizing multiline regexp. I have made some ruby code that stresses it.
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Ruby.tmbundle@1413 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Test/division_ambiguity.rb
@@ -0,0 +1,65 @@
+# purpose:
+# exercise constructs with division
+#
+# division itself      84 / 2
+# regexp               /pattern/
+#
+#
+#
+
+def test(obj)
+	p obj
+end
+a, b = 4, 2
+
+# -------------------------------------------
+#
+# Testarea for division
+#
+# -------------------------------------------
+
+# singleline numbers
+test(84 / 2)
+
+# singleline symbols
+test(a / b)
+
+# singleline symbols
+test(a / b / 3)   # TODO: not recognize as regexp
+
+
+# multiline with symbols
+=begin # invalid
+test(a 
+/ b)
+=end
+
+# multiline with symbols
+test(a / 
+b)
+
+
+# -------------------------------------------
+#
+# Testarea for regexp
+#
+# -------------------------------------------
+
+# singleline
+test( // )
+test( /abc/ )
+test( /a\/bc/ )
+
+
+# multiline  TODO: color me!
+test( /
+pattern
+/x    )
+
+
+# multiline  TODO: color me!
+test( 
+/r
+eg
+e/x
+)

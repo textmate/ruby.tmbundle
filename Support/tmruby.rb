0b28f6b4d6ede5e1106338ecfa2cd977c33339ae
Sune Foldager <cryo@svn.textmate.org>
2005-08-12 18:52:15 +0000

    Added TM-Ruby which is a ruby runtime similar to PyMate. Note the following:
    • This is work in progress :-p.
    • The design was completely ripped off from PyMate, although I factored it out in a CSS file.
    • There is no output processing, in particular there are no clickable links on errors etc. Soon :-).
    • There is some input processing, popping up dialogs to handle input. The 'Abort' button doesn't work yet.
    • Feedback and suggestions: IRC (cyanite) or mail :-p.
    
    
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Ruby.tmbundle@1512 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Support/tmruby.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/ruby
+#
+# TM-Ruby v0.1, 12-08-2005.
+# By Sune Foldager.
+#
+
+
+# Input override.
+class MyStdIn
+
+  def getLine(info)
+    s = `\"#{ENV['TM_SUPPORT_PATH']}/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog\" inputbox --title Input --informative-text '#{info}' --button1 Ok --button2 '^D' --button3 'Abort'`
+    case (a = s.split)[0].to_i
+    when 1: a[1] + "\n" if a[1]
+    when 2: nil
+    when 3: nil # <- for now.
+    end
+  end
+
+  def gets(sep = nil)
+    getLine('Line input.')
+  end
+
+end
+$stdin = MyStdIn.new
+
+
+# Helper to dump files to stdout.
+def dump_file(name)
+  File.open(name) {|f| f.each_line {|l| print l} }
+end
+
+
+
+# Prepare some values for later.
+myFile = __FILE__
+myDir = File.dirname(myFile) + '/'
+
+
+# Headers...
+
+print <<EOF
+<html>
+<head>
+<title>Ruby TextMate Runtime</title>
+<style type="text/css">
+EOF
+
+dump_file(myDir + 'pastel.css')
+
+print <<EOF
+</style>
+</head>
+<body>
+<div id="script_output">
+<pre><strong>TM-Ruby v0.1 running Ruby v#{RUBY_VERSION}.</strong>
+<strong>&gt;&gt;&gt #{ARGV[0]}</strong>
+
+EOF
+
+
+# Load and execute the user code.
+begin
+  load ARGV[0]
+rescue Exception => e
+
+  # For now.
+  puts e.message
+  puts e.inspect.sub('<', '&lt;').sub('>', '&gt;')
+
+  # Filter backtrace.
+  bt = e.backtrace
+  bt = bt[0...(bt.each_index {|i| break i if bt[i].index(__FILE__) == 0 })]
+  puts bt.join("\n")
+
+end
+
+
+# Footer.
+print <<EOF
+</pre>
+</div>
+</body>
+</html>
+EOF
+

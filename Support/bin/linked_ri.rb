a7641957ac51ed7d604316e80a5e8882a1896adf
James Edward Gray II <james@grayproductions.net>
2006-07-07 02:49:56 +0000

    Switching to RI's text output since RI's HTML is very, very busted.  This is a first step and probably not perfect.  Report bugs as you find them...
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Ruby.tmbundle@4035 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/bin/linked_ri.rb
+++ Support/bin/linked_ri.rb
@@ -1,45 +1,50 @@
 #!/usr/bin/env ruby -w
 
-# if we are not called directly from TM (e.g. JavaScript) the caller should ensure that RUBYLIB is set properly
+# if we are not called directly from TM (e.g. JavaScript) the caller
+# should ensure that RUBYLIB is set properly
 $: << "#{ENV["TM_SUPPORT_PATH"]}/lib" if ENV.has_key? "TM_SUPPORT_PATH"
 require "escape"
+
+require "erb"
+include ERB::Util
 
 term = ARGV.shift
 
 def link_methods(prefix, methods)
-  methods.gsub(/<\/?\w+>/, "").split(/,\s*/).map do |meth|
-    "<a href=\"javascript:ri('#{prefix}#{meth}')\">#{meth}</a>"
-  end.join(", ")
+  methods.split(/(,\s*)/).map do |match|
+    match[0] == ?, ?
+      match : "<a href=\"javascript:ri('#{prefix}#{match}')\">#{match}</a>"
+  end.join
 end
 
-documentation = `ri -Tf html #{e_sh term}` rescue "<h1>ri Command Error.</h1>"
+documentation = h(`ri -T #{e_sh term}`) rescue "<h1>ri Command Error.</h1>"
+
+documentation.gsub!(/(\s|^)\+(\w+)\+(\s|$)/, "\\1<code>\\2</code>\\3")
 
 if documentation.include? "More than one method matched"
-  methods       = documentation.to_a.last.split(/,\s*/)
+  methods       = documentation.split(/\n[ \t]*\n/).last.strip.split(/,\s*/)
   list_items    = methods.inject("") do |str, item|
     str + "<li><a href=\"javascript:ri('#{item}')\">#{item}</a></li>\n"
   end
   documentation = "<h1>Multiple Matches:</h1>\n<ul>\n#{list_items}</ul>\n"
-elsif documentation.sub!( /\A\s*<b>([A-Z_]\w*)(#|::|\.)/,
-                          "<b><a href=\"javascript:ri('\\1')\">\\1</a>\\2" )
+elsif documentation.sub!( /\A(-+\s+)([A-Z_]\w*)(#|::|\.)/,
+                          "\\1<a href=\"javascript:ri('\\2')\">\\2</a>\\3" )
   # do nothing--added class/module link
 else
-  documentation.sub!( /\A\s*(<b>Class: \w* < )([^\s<]+)/,
+  documentation.sub!( /\A(-+\s+Class: \w* < )([^\s<]+)/,
                             "\\1<a href=\"javascript:ri('\\2')\">\\2</a>" )
-  documentation.sub!(/<h2>Includes:<\/h2>\s+(.+)$/) do
-    "<h2>Includes:</h2>\n" +
-    "<p>" + $1.gsub(/<\/?\w+>/, "").gsub(/([A-Z_]\w*)\(([^)]*)\)/) do |match|
+  documentation.sub!(/(Includes:\s+-+\s+)(.+?)([ \t]*\n[ \t]*\n|\s*\Z)$/m) do
+    head, meths, foot = $1, $2, $3
+    head + meths.gsub(/([A-Z_]\w*)\(([^)]*)\)/) do |match|
       "<a href=\"javascript:ri('#{$1}')\">#{$1}</a>(" +
       link_methods("#{$1}#", $2) + ")"
-    end + "</p>\n"
+    end + foot
   end
-  documentation.sub!(/<h2>Class methods:<\/h2>\s+(.+)$/) do
-    "<h2>Class methods:</h2>\n" +
-    "<p>" + link_methods("#{term}::", $1) + "</p>\n"
+  documentation.sub!(/(Class methods:\s+-+\s+)(.+?)([ \t]*\n[ \t]*\n|\s*\Z)$/m) do
+    $1 + link_methods("#{term}::", $2) + $3
   end
-  documentation.sub!(/<h2>Instance methods:<\/h2>\s+(.+)$/) do
-    "<h2>Instance methods:</h2>\n" +
-    "<p>" + link_methods("#{term}#", $1) + "</p>\n"
+  documentation.sub!(/(Instance methods:\s+-+\s+)(.+?)([ \t]*\n[ \t]*\n|\s*\Z)$/m) do
+    $1 + link_methods("#{term}#", $2) + $3
   end
 end
 

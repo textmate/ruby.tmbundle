5b37361bef6391b21ced029000fc95d8e4ff1fcd
Stanley Rost <Soryu@svn.textmate.org>
2007-06-13 22:37:17 +0000

    • Remove ri.rb script, merged with linked_ri.rb • History for initial term is still broken if it was ambiguous
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Ruby.tmbundle@7518 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/bin/linked_ri.rb
+++ Support/bin/linked_ri.rb
@@ -3,13 +3,22 @@
 # if we are not called directly from TM (e.g. JavaScript) the caller
 # should ensure that RUBYLIB is set properly
 $: << "#{ENV["TM_SUPPORT_PATH"]}/lib" if ENV.has_key? "TM_SUPPORT_PATH"
-require "escape"
+LINKED_RI = "#{ENV["TM_BUNDLE_SUPPORT"]}/bin/linked_ri.rb"
+
+require "exit_codes"
+require "ui"
+require "web_preview"
 
 require "enumerator"
 require "erb"
 include ERB::Util
 
 term = ARGV.shift
+
+# first escape for use in the shell, then escape for use in a JS string
+def e_js_sh(str)
+  (e_sh str).gsub("\\", "\\\\\\\\")
+end
 
 def link_methods(prefix, methods)
   methods.split(/(,\s*)/).map do |match|
@@ -18,36 +27,93 @@ def link_methods(prefix, methods)
   end.join
 end
 
-documentation = h(`ri -T #{e_sh term}`) rescue "<h1>ri Command Error.</h1>"
-
-documentation.gsub!(/(\s|^)\+(\w+)\+(\s|$)/, "\\1<code>\\2</code>\\3")
-
-if documentation.include? "More than one method matched"
-  methods       = documentation.split(/\n[ \t]*\n/).last.strip.split(/,\s*/)
-  documentation = ">> #{methods.join(' ')}"
-elsif documentation =~ /\A(?:-+\s+)((?:[A-Z_]\w*::)*[A-Z_]\w*)(#|::|\.)/
-  nesting   = $1
-  constants = nesting.split("::")
-  linked    = constants.enum_with_index.map do |const, i|
-    "<a href=\"javascript:ri('#{constants[0..i].join('::')}')\">#{const}</a>"
-  end
-  documentation.sub!(nesting, linked.join("::"))
-else
-  documentation.sub!( /\A(-+\s+Class: \w* < )([^\s<]+)/,
-                            "\\1<a href=\"javascript:ri('\\2')\">\\2</a>" )
-  documentation.sub!(/(Includes:\s+-+\s+)(.+?)([ \t]*\n[ \t]*\n|\s*\Z)/m) do
-    head, meths, foot = $1, $2, $3
-    head + meths.gsub(/([A-Z_]\w*)\(([^)]*)\)/) do |match|
-      "<a href=\"javascript:ri('#{$1}')\">#{$1}</a>(" +
-      link_methods("#{$1}#", $2) + ")"
-    end + foot
-  end
-  documentation.sub!(/(Class methods:\s+-+\s+)(.+?)([ \t]*\n[ \t]*\n|\s*\Z)/m) do
-    $1 + link_methods("#{term}::", $2) + $3
-  end
-  documentation.sub!(/(Instance methods:\s+-+\s+)(.+?)([ \t]*\n[ \t]*\n|\s*\Z)/m) do
-    $1 + link_methods("#{term}#", $2) + $3
+def ri(term)
+  documentation = `#{e_sh LINKED_RI} '#{term}' 'js' 2>&1` \
+                  rescue "<h1>ri Command Error.</h1>"
+  if documentation =~ /Nothing known about /
+    TextMate.exit_show_tool_tip(documentation)
+  elsif documentation.sub!(/\A>>\s*/, "")
+    choices = documentation.split
+    choice  = TextMate::UI.menu(choices)
+    exit if choice.nil?
+    ri(choices[choice])
+  else
+    documentation
   end
 end
 
-puts documentation.gsub("\n", "<br />")
+mode = ARGV.shift
+if mode.nil? then
+
+  term = STDIN.read.strip
+  TextMate.exit_show_tool_tip("Please select a term to look up.") if term.empty?
+
+  documentation = ri(term)
+
+  html_header("Documentation for ‘#{term}’", "RDoc", <<-HTML)
+<script type="text/javascript" charset="utf-8">
+  function ri (arg, _history) {
+  TextMate.isBusy = true;
+  var res = TextMate.system("RUBYLIB=#{e_js_sh "#{ENV['TM_SUPPORT_PATH']}/lib"} #{e_js_sh LINKED_RI} 2>&1 '" + arg + "' 'js'", null).outputString;
+  document.getElementById("actual_output").innerHTML = res;
+  TextMate.isBusy = false;
+  if(!_history)
+  {
+    var history = document.getElementById('search_history');
+    var new_option = document.createElement('option');
+    new_option.setAttribute('value', arg);
+    new_option.appendChild(document.createTextNode(arg));
+    history.appendChild(new_option);
+  }
+}
+</script>
+HTML
+  puts <<-HTML
+<select id="search_history" style="float: right;">
+  <option value="#{term}" selected="selected">#{term}</option>
+</select>
+<script type="text/javascript" charset="utf-8">
+  document.getElementById('search_history').addEventListener('change', function(e) {
+    ri(document.getElementById('search_history').value, true);
+  }, false);
+</script>
+<div id="actual_output">#{documentation}</div>
+HTML
+  html_footer
+  TextMate.exit_show_html
+elsif mode == 'js' then
+  
+  documentation = h(`ri -T #{e_sh term}`) rescue "<h1>ri Command Error.</h1>"
+
+  documentation.gsub!(/(\s|^)\+(\w+)\+(\s|$)/, "\\1<code>\\2</code>\\3")
+
+  if documentation.include? "More than one method matched"
+    methods       = documentation.split(/\n[ \t]*\n/).last.strip.split(/,\s*/)
+    documentation = ">> #{methods.join(' ')}"
+  elsif documentation =~ /\A(?:-+\s+)((?:[A-Z_]\w*::)*[A-Z_]\w*)(#|::|\.)/
+    nesting   = $1
+    constants = nesting.split("::")
+    linked    = constants.enum_with_index.map do |const, i|
+      "<a href=\"javascript:ri('#{constants[0..i].join('::')}')\">#{const}</a>"
+    end
+    documentation.sub!(nesting, linked.join("::"))
+  else
+    documentation.sub!( /\A(-+\s+Class: \w* < )([^\s<]+)/,
+                              "\\1<a href=\"javascript:ri('\\2')\">\\2</a>" )
+    documentation.sub!(/(Includes:\s+-+\s+)(.+?)([ \t]*\n[ \t]*\n|\s*\Z)/m) do
+      head, meths, foot = $1, $2, $3
+      head + meths.gsub(/([A-Z_]\w*)\(([^)]*)\)/) do |match|
+        "<a href=\"javascript:ri('#{$1}')\">#{$1}</a>(" +
+        link_methods("#{$1}#", $2) + ")"
+      end + foot
+    end
+    documentation.sub!(/(Class methods:\s+-+\s+)(.+?)([ \t]*\n[ \t]*\n|\s*\Z)/m) do
+      $1 + link_methods("#{term}::", $2) + $3
+    end
+    documentation.sub!(/(Instance methods:\s+-+\s+)(.+?)([ \t]*\n[ \t]*\n|\s*\Z)/m) do
+      $1 + link_methods("#{term}#", $2) + $3
+    end
+  end
+
+  puts documentation.gsub("\n", "<br />")
+end

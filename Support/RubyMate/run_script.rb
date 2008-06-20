6f1158b5d569d924b225d9362708bea7c5aed156
Luke Daley <ld@ldaley.com>
2008-06-20 05:59:19 +0000

    • Transitioned from scriptmate to TextMate::Executor.run().
    Every attempt was made to preserve the original logic.
    
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Ruby.tmbundle@9980 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/RubyMate/run_script.rb
+++ Support/RubyMate/run_script.rb
@@ -1,76 +1,64 @@
-require "#{ENV["TM_SUPPORT_PATH"]}/lib/scriptmate"
-require "cgi"
+require ENV["TM_SUPPORT_PATH"] + "/lib/tm/executor"
+require ENV["TM_SUPPORT_PATH"] + "/lib/tm/save_current_document"
 
-$SCRIPTMATE_VERSION = "$Revision$"
+TextMate.save_current_document
 
-class RubyScript < UserScript
-  def lang; "Ruby" end
-  def default_extension; ".rb" end
-  def executable; @hashbang || ENV['TM_RUBY'] || 'ruby' end
-  def args; ['-rcatch_exception'] end
-  def version_string
-    ruby_path = %x{ #{executable} -e 'require "rbconfig"; print Config::CONFIG["bindir"] + "/" + Config::CONFIG["ruby_install_name"]'}
-    res = "Ruby r" + %x{ #{executable} -e 'print RUBY_VERSION' }
-    res + " (#{ruby_path})"
+is_test_script = ENV["TM_FILEPATH"] =~ /(?:\b|_)(?:tc|ts|test)(?:\b|_)/ or
+  File.read(ENV["TM_FILEPATH"]) =~ /\brequire\b.+(?:test\/unit|test_helper)/
+
+cmd = [ENV['TM_RUBY'] || 'ruby', '-rcatch_exception']
+
+if is_test_script and not ENV['TM_FILE_IS_UNTITLED']
+  path_ary = (ENV['TM_ORIG_FILEPATH'] || ENV['TM_FILEPATH']).split("/")
+  if index = path_ary.rindex("test")
+    test_path = File.join(*path_ary[0..-2])
+    lib_path  = File.join( *( path_ary[0..-2] +
+                              [".."] * (path_ary.length - index - 1) ) +
+                              ["lib"] )
+    if File.exist? lib_path
+      cmd << "-I#{lib_path}:#{test_path}"
+    end
   end
-  def test_script?
-    @path    =~ /(?:\b|_)(?:tc|ts|test)(?:\b|_)/ or
-    @content =~ /\brequire\b.+(?:test\/unit|test_helper)/
-  end
-  def filter_cmd(cmd)
-    if test_script?
-      path_ary = @path.split("/")
-      if index = path_ary.rindex("test")
-        test_path = File.join(*path_ary[0..-2])
-        lib_path  = File.join( *( path_ary[0..-2] +
-                                  [".."] * (path_ary.length - index - 1) ) +
-                                  ["lib"] )
-        if File.exist? lib_path
-          cmd.insert(1, "-I#{e_sh lib_path}:#{e_sh test_path}")
+end
+
+cmd << ENV["TM_FILEPATH"]
+
+TextMate::Executor.run(cmd, :version_args => ["--version"]) do |str, type|
+  case type
+  when :out
+    if is_test_script and str =~ /\A[.EF]+\Z/
+      htmlize(str).gsub(/[EF]+/, "<span style=\"color: red\">\\&</span>") +
+            "<br style=\"display: none\"/>"
+    elsif is_test_script
+      out = str.map do |line|
+        if line =~ /^(\s+)(\S.*?):(\d+)(?::in\s*`(.*?)')?/
+          indent, file, line, method = $1, $2, $3, $4
+          url, display_name = '', 'untitled document';
+          unless file == "-"
+            indent += " " if file.sub!(/^\[/, "")
+            url = '&amp;url=file://' + e_url(file)
+            display_name = File.basename(file)
+          end
+          "#{indent}<a class='near' href='txmt://open?line=#{line + url}'>" +
+          (method ? "method #{CGI::escapeHTML method}" : '<em>at top level</em>') +
+          "</a> in <strong>#{CGI::escapeHTML display_name}</strong> at line #{line}<br/>"
+        elsif line =~ /(\[[^\]]+\]\([^)]+\))\s+\[([\w\_\/\.]+)\:(\d+)\]/
+          spec, file, line = $1, $2, $3, $4
+          "<span><a style=\"color: blue;\" href=\"txmt://open?url=file://#{e_url(file)}&amp;line=#{line}\">#{spec}</span>:#{line}<br/>"
+        elsif line =~ /([\w\_]+).*\[([\w\_\/\.]+)\:(\d+)\]/
+          method, file, line = $1, $2, $3
+          "<span><a style=\"color: blue;\" href=\"txmt://open?url=file://#{e_url(file)}&amp;line=#{line}\">#{method}</span>:#{line}<br/>"
+        elsif line =~ /^\d+ tests, \d+ assertions, (\d+) failures, (\d+) errors\b.*/
+          "<span style=\"color: #{$1 + $2 == "00" ? "green" : "red"}\">#{$&}</span><br/>"
+        else
+          htmlize(line)
         end
       end
-    end
-    cmd
-  end
-end
-
-class RubyMate < ScriptMate
-  def filter_stdout(str)
-    if @command.test_script? and str =~ /\A[.EF]+\Z/
-      return htmlize(str).gsub(/[EF]+/, "<span style=\"color: red\">\\&</span>") +
-            "<br style=\"display: none\"/>"
+      out.join()
     else
-      if @command.test_script?
-        return ( str.map do |line|
-          if line =~ /^(\s+)(\S.*?):(\d+)(?::in\s*`(.*?)')?/
-            indent, file, line, method = $1, $2, $3, $4
-            url, display_name = '', 'untitled document';
-            unless file == "-"
-              indent += " " if file.sub!(/^\[/, "")
-              url = '&amp;url=file://' + e_url(file)
-              display_name = File.basename(file)
-            end
-            "#{indent}<a class='near' href='txmt://open?line=#{line + url}'>" +
-            (method ? "method #{CGI::escapeHTML method}" : '<em>at top level</em>') +
-            "</a> in <strong>#{CGI::escapeHTML display_name}</strong> at line #{line}<br/>"
-          elsif line =~ /(\[[^\]]+\]\([^)]+\))\s+\[([\w\_\/\.]+)\:(\d+)\]/
-            spec, file, line = $1, $2, $3, $4
-            "<span><a style=\"color: blue;\" href=\"txmt://open?url=file://#{e_url(file)}&amp;line=#{line}\">#{spec}</span>:#{line}<br/>"
-          elsif line =~ /([\w\_]+).*\[([\w\_\/\.]+)\:(\d+)\]/
-            method, file, line = $1, $2, $3
-            "<span><a style=\"color: blue;\" href=\"txmt://open?url=file://#{e_url(file)}&amp;line=#{line}\">#{method}</span>:#{line}<br/>"
-          elsif line =~ /^\d+ tests, \d+ assertions, (\d+) failures, (\d+) errors\b.*/
-            "<span style=\"color: #{$1 + $2 == "00" ? "green" : "red"}\">#{$&}</span><br/>"
-          else
-            htmlize(line)
-          end
-        end.join )
-      else
-        return htmlize(str)
-      end
-    end
+      htmlize(str)
+    end    
+  when :err
+    "<span style=\"color: red\">#{htmlize str}</span>"
   end
 end
-
-script = RubyScript.new(STDIN.read)
-RubyMate.new(script).emit_html

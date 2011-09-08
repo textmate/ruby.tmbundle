84072d00d5737b7c8eb2f03fa3c3ec18274df4a7
Michael Sheets <git@whitefalls.org>
2011-09-08 04:28:49 -0500

    Execute using TM_RUBY.

---
+++ Support/bin/execute_line.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# be smart, dont print something if we already have..
+$write_count = 0
+def STDOUT.write(what)
+   $write_count += 1
+   super(what)
+end
+
+# execure the code
+begin
+  # insert a space if input was a selection, if it was a line insert \n
+  print(ENV['TM_SELECTED_TEXT'] ? " " : "\n")
+  r = eval(STDIN.read)
+rescue Object
+  r = $!.class.to_s
+end
+
+# try to_s, if it doesnt work use inspect
+# Array and Hash are shown via inspect because they look better with formating
+# do this just if the script did not print anything itself
+if $write_count == 1
+  print( (r.class != Hash and r.class != Array and not r.nil? and r.respond_to? :to_s) ? r.to_s : r.inspect )
+  print( "\n" ) unless ENV.has_key?('TM_SELECTED_TEXT')
+end
